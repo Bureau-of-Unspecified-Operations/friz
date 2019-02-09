b1e36c7ada2f135b40dd01c75da25d3e53ca552c
jecneps <jejcneps@gmail.com>
2019-02-09 09:27:47 -0500

    outline peer

---
+++ peer.py
@@ -0,0 +1,12 @@
+
+def searchSessionRequest():
+    pass
+
+def commitSessionRequest(req):
+    pass
+
+def launchTorPayload(tor, payload):
+    pass
+
+def createRotPayload(pub, payload):
+    pass
