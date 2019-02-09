d4dfefb6597b4e6f8280e3ea2fe2e909e5af3dae
jecneps <jejcneps@gmail.com>
2019-02-09 15:19:31 -0500

    tor

--- tor_server.py
+++ tor_server.py
@@ -13,16 +13,16 @@ class TORServer(RPCProtocol):
     def rpc_forward(self, sender, data):
         print(sender)
         print(data)
+        if TOR.isTorPacket(data):
+            TOR.forwardPacket(data, self)
         return "packet recieved"
     
-
 class TOR(object):        
     def __init__(self, loop, port):
         listen = loop.create_datagram_endpoint(TORServer, local_addr=('127.0.0.1', port))
         transport, protocol = loop.run_until_complete(listen)
         self.protocol = protocol
-        self.loop = loop
-        
+        '''
     @asyncio.coroutine
     def forward(self, protocol, address):
         # result will be a tuple - first arg is a boolean indicating whether a response
@@ -33,6 +33,27 @@ class TOR(object):
     def forwardTOR(self):
         func = self.forward(self.protocol, ('127.0.0.1', 1234))
         self.loop.run_until_complete(func)
+'''
+    def forwardPacketSelf(self, packet):
+        func = TOR.forward(self.protocol, TOR.addressFromPacket(packet),
+                           TOR.dataFromPacket)
+
+    @asyncio.coroutine
+    def forward(protocol, address, data):
+        result = yield from protocol.forward(address, data)
+        print(result[1] if result[0] else "No response received.")
+
+    def forwardPacket(protocol, packet):
+        #extract shit
+        func = TOR.forward(protocol, address, data)
+        asyncio.get_event_loop().run_until_complete(func)
+        
+
+    def isTorPacket(packet):
+        return True
+
+    def addressFromPacket(packet):
+        pass
 
 
 
