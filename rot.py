1020c28517c9e0ed1e764795b133eace6bfd1192
anythingthatworks <omr@andrew.cmu.edu>
2019-02-09 11:42:51 -0500

    orson1

--- rot.py
+++ rot.py
@@ -2,19 +2,62 @@
 #############################################
 # INTERFACE TO PEERS
 #############################################
+import rsa;
+import random;
 
-
-def formROT(payload, trgPub):
-    # connect to DHT, randomly select ORs, get IP and KEY data
-    # layer on encyrption
+def Network():
+    userList;
+    def __init__(num):
+        if(num<1):
+            num = 1
+        for i in range(num):
+            userList.append(User())
     pass
 
-# takes rot packet and payload, extracts 1st trg IP, and send rot+payload on it's way
-def sendROT(rot, payload):
+    def addUser(User):
+        user.append(User)
     pass
 
-##############################################
+    #probably remove this eventually
+    def getUserNum(num):
+        if(num>userList.len()):
+            num = userList.len()-1
+        if(userList.len()>num and num>0):
+            return userList[num]
+        return None
+
+    def getUserPub(num):
+        for i in range(userList.len()):
+            if(userList[i].msgPubKey):
+                return None
+        return None 
+
+    def getRandom():
+        out
+        len = random.randint(3,6)
+        for i in range(len):
+             out.apped(userList[random.randint(0,userList.len()-1)])
+        return out
 
 
-def forwardROT(packet):
-    pass
+def User():
+    msgPubKey, msgPrivKey, namePubKey, namePrivKey
+    def __init__():
+        (msgPubKey, msgPrivKey) = rsa.newkeys(512)
+        (namePubKey, namePrivKey) = rsa.newkeys(512)
+        pass
+    
+    def formROT(payload, trgPub, Network):
+            path = Network.getRandom()
+            pass
+
+
+    # takes rot packet and payload, extracts 1st trg IP, and send rot+payload on it's way
+    def sendROT(rot, payload):
+        pass
+
+    def forwardROT(packet):
+        pass
+
+net = Network(5);
+net.userList[1]
