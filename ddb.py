c4bf557062fc040ce3697c04f0dfd461515813fa
jecneps <jejcneps@gmail.com>
2019-02-09 00:30:20 -0500

    started outline

---
+++ ddb.py
@@ -0,0 +1,15 @@
+
+#############################################
+# INTERFACE TO PEERS
+#############################################
+
+# takes a req of form pub(ROT_packet), adds to DDB
+def putSessionRequest(req):
+    pass
+
+# gives you a random (IP, pub) pair from the network
+def getOnionRouter():
+    pass
+
+
+##############################################
---
+++ rot.py
@@ -0,0 +1,20 @@
+
+#############################################
+# INTERFACE TO PEERS
+#############################################
+
+
+def formROT(payload, trgPub):
+    # connect to DHT, randomly select ORs, get IP and KEY data
+    # layer on encyrption
+    pass
+
+# takes rot packet and payload, extracts 1st trg IP, and send rot+payload on it's way
+def sendROT(rot, payload):
+    pass
+
+##############################################
+
+
+def forwardROT(packet):
+    pass
